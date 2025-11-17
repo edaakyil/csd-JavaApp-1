2b7b080d374f990f319819dcaa6ecb979154097a
edaakyil <edaakyil@gmail.com>
2025-11-17 15:58:43 +0300

    Update

--- Projects/012-SpringBoot/001-DemoCoreAnnotations/src/main/java/com/edaakyil/java/app/component/StartDateTimeDisplay.java
+++ Projects/012-SpringBoot/001-DemoCoreAnnotations/src/main/java/com/edaakyil/java/app/component/StartDateTimeDisplay.java
@@ -12,6 +12,11 @@ public class StartDateTimeDisplay {
     private final LocalDateTime m_startDateTime;
     private final DateTimeFormatter m_dateTimeFormatter;
 
+    // StartDateTimeDisplay'e LocalDateTime ve DateTimeFormatter enjekte ettik.
+    // constructor ile yapılan bu enjeckte işlemine dependency injection literatüründe "constructor injection" denilir.
+    // StartDateTimeDisplay sınıfına constructor injection yöntemiyle LocalDateTime ve DateTimeFormatter'ı
+    // DateTimeConfig'deki startDateTime metodundan LocalDateTime ve DateTimeFormatterConfig'deki createDateTimeFormatter
+    // metodundan DateTimeFormatter'ı elde ettik.
     public StartDateTimeDisplay(LocalDateTime startDateTime, DateTimeFormatter dateTimeFormatter)
     {
         m_startDateTime = startDateTime;
@@ -23,7 +28,7 @@ public class StartDateTimeDisplay {
     @PostConstruct
     public void displayStartDateTime()
     {
-        Console.writeLine("StartDateTimeDisplay - displayStartDateTime: %s", m_dateTimeFormatter.format(m_startDateTime));
+        //Console.writeLine("StartDateTimeDisplay - displayStartDateTime: %s", m_dateTimeFormatter.format(m_startDateTime));
         Console.writeLine("StartDateTimeDisplay - displayStartDateTime: %s", m_startDateTime.format(m_dateTimeFormatter));
     }
 }
