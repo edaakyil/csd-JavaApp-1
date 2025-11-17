a8bc2f2e7e0280b10ceab0874fa46a6fd265195c
edaakyil <edaakyil@gmail.com>
2025-11-17 18:32:31 +0300

    Update

--- Projects/012-SpringBoot/001-DemoCoreAnnotations/src/main/java/com/edaakyil/java/app/App.java
+++ Projects/012-SpringBoot/001-DemoCoreAnnotations/src/main/java/com/edaakyil/java/app/App.java
@@ -2,8 +2,10 @@ package com.edaakyil.java.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.edaakyil")
 public class App {
 
 	public static void main(String[] args) {
