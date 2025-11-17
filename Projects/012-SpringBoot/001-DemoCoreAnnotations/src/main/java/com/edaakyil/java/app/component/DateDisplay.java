283f04c52ca60275efca62e4d59134ff181b0234
edaakyil <edaakyil@gmail.com>
2025-11-17 15:03:08 +0300

    Update

--- Projects/012-SpringBoot/001-DemoCoreAnnotations/src/main/java/com/edaakyil/java/app/component/DateDisplay.java
+++ Projects/012-SpringBoot/001-DemoCoreAnnotations/src/main/java/com/edaakyil/java/app/component/DateDisplay.java
@@ -1,12 +1,24 @@
 package com.edaakyil.java.app.component;
 
 import com.karandev.io.util.console.Console;
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @Component
 public class DateDisplay {
     public DateDisplay()
     {
         Console.writeLine("default ctor of DateDisplay");
+    }
+
+    @PostConstruct
+    public void displayLocalDate()
+    {
+        var today = LocalDate.now();
+
+        Console.writeLine("DateDisplay: %s", today.format(DateTimeFormatter.ISO_DATE));
     }
 }
