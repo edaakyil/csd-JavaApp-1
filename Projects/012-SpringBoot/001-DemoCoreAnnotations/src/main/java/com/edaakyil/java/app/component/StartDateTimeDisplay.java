7ebcd5a113425f413ebe43cafc05237507e1cb75
edaakyil <edaakyil@gmail.com>
2025-11-17 17:06:24 +0300

    Update

--- Projects/012-SpringBoot/001-DemoCoreAnnotations/src/main/java/com/edaakyil/java/app/component/StartDateTimeDisplay.java
+++ Projects/012-SpringBoot/001-DemoCoreAnnotations/src/main/java/com/edaakyil/java/app/component/StartDateTimeDisplay.java
@@ -2,6 +2,7 @@ package com.edaakyil.java.app.component;
 
 import com.karandev.io.util.console.Console;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -12,12 +13,10 @@ public class StartDateTimeDisplay {
     private final LocalDateTime m_startDateTime;
     private final DateTimeFormatter m_dateTimeFormatter;
 
-    // StartDateTimeDisplay'e LocalDateTime ve DateTimeFormatter enjekte ettik.
+    // StartDateTimeDisplay'e constructor injection yöntemi ile LocalDateTime ve DateTimeFormatter türlerini enjekte ettik.
     // constructor ile yapılan bu enjeckte işlemine dependency injection literatüründe "constructor injection" denilir.
-    // StartDateTimeDisplay sınıfına constructor injection yöntemiyle LocalDateTime ve DateTimeFormatter'ı
-    // DateTimeConfig'deki startDateTime metodundan LocalDateTime ve DateTimeFormatterConfig'deki createDateTimeFormatter
-    // metodundan DateTimeFormatter'ı elde ettik.
-    public StartDateTimeDisplay(LocalDateTime startDateTime, DateTimeFormatter dateTimeFormatter)
+    public StartDateTimeDisplay(@Qualifier("com.edaakyil.datetime.start") LocalDateTime startDateTime,
+                                DateTimeFormatter dateTimeFormatter)
     {
         m_startDateTime = startDateTime;
         m_dateTimeFormatter = dateTimeFormatter;
--- Projects/012-SpringBoot/001-DemoCoreAnnotations/src/main/java/com/edaakyil/java/app/configuration/DateTimeConfig.java
+++ Projects/012-SpringBoot/001-DemoCoreAnnotations/src/main/java/com/edaakyil/java/app/configuration/DateTimeConfig.java
@@ -3,15 +3,25 @@ package com.edaakyil.java.app.configuration;
 import com.karandev.io.util.console.Console;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 import java.time.LocalDateTime;
 
 @Configuration
 public class DateTimeConfig {
-    @Bean
+    @Bean("com.edaakyil.datetime.start")
     public LocalDateTime startDateTime()
     {
-        Console.writeLine("bean of LocalDateTime in DateTimeConfig");
+        Console.writeLine("bean of LocalDateTime in DateTimeConfig - startDateTime");
+
+        return LocalDateTime.now();
+    }
+
+    @Bean("com.edaakyil.datetime.now")
+    @Scope("prototype")
+    public LocalDateTime currentDateTime()
+    {
+        Console.writeLine("bean of LocalDateTime in DateTimeConfig - currentDateTime");
 
         return LocalDateTime.now();
     }
