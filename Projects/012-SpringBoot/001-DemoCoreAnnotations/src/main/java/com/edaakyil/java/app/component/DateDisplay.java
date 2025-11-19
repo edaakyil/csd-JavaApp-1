43b63ba319f0748fe39df3de6ffca6307b695bff
edaakyil <edaakyil@gmail.com>
2025-11-19 14:03:43 +0300

    Update

--- Projects/012-SpringBoot/001-DemoCoreAnnotations/src/main/java/com/edaakyil/java/app/component/DateDisplay.java
+++ Projects/012-SpringBoot/001-DemoCoreAnnotations/src/main/java/com/edaakyil/java/app/component/DateDisplay.java
@@ -2,23 +2,33 @@ package com.edaakyil.java.app.component;
 
 import com.karandev.io.util.console.Console;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+import static com.edaakyil.spring.lib.datetime.constant.BeanName.DATE_FORMATTER_TR_BEAN;
+
 @Component
 public class DateDisplay {
     private final LocalDate m_localDate;
+    private final DateTimeFormatter m_formatter;
 
-    public DateDisplay(LocalDate localDate)
+    @Value("${app.message.date.current:Date}")
+    private String m_message;
+
+    public DateDisplay(LocalDate localDate,
+                       @Qualifier(DATE_FORMATTER_TR_BEAN) DateTimeFormatter formatter)
     {
         m_localDate = localDate;
+        m_formatter = formatter;
     }
 
     @PostConstruct
     public void displayLocalDate()
     {
-        Console.writeLine("DateDisplay: %s", m_localDate.format(DateTimeFormatter.ISO_DATE));
+        Console.writeLine("%s: %s", m_message, m_localDate.format(m_formatter));
     }
 }
--- Projects/012-SpringBoot/001-DemoCoreAnnotations/src/main/java/com/edaakyil/java/app/component/StartDateTimeDisplay.java
+++ Projects/012-SpringBoot/001-DemoCoreAnnotations/src/main/java/com/edaakyil/java/app/component/StartDateTimeDisplay.java
@@ -3,6 +3,7 @@ package com.edaakyil.java.app.component;
 import com.karandev.io.util.console.Console;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -15,6 +16,11 @@ import static com.edaakyil.spring.lib.datetime.constant.BeanName.LOCAL_START_DAT
 public class StartDateTimeDisplay {
     private final LocalDateTime m_startDateTime;
     private final DateTimeFormatter m_dateTimeFormatter;
+
+    // Property injection
+    // .properties dosyasından otomatik olarak enjekte etmesi için @Value annotation'ı kullanılır
+    @Value("${app.message.datetime.start}")
+    private String m_message;
 
     // StartDateTimeDisplay'e constructor injection yöntemi ile LocalDateTime ve DateTimeFormatter türlerini enjekte ettik.
     // constructor ile yapılan bu enjeckte işlemine dependency injection literatüründe "constructor injection" denilir.
@@ -29,6 +35,6 @@ public class StartDateTimeDisplay {
     public void displayStartDateTime()
     {
         //Console.writeLine("StartDateTimeDisplay: %s", m_dateTimeFormatter.format(m_startDateTime));
-        Console.writeLine("StartDateTimeDisplay: %s", m_startDateTime.format(m_dateTimeFormatter));
+        Console.writeLine("%s: %s", m_message, m_startDateTime.format(m_dateTimeFormatter));
     }
 }
