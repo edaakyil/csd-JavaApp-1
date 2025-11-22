b651ce940aa6ca68f34909274039bfa4836478a1
edaakyil <edaakyil@gmail.com>
2025-11-22 14:06:35 +0300

    Update

--- Projects/012-SpringBoot/004-DemoCollectionInjectionApp/OperationLib/src/main/java/com/edaakyil/spring/lib/operation/component/AddOperation.java
+++ Projects/012-SpringBoot/004-DemoCollectionInjectionApp/OperationLib/src/main/java/com/edaakyil/spring/lib/operation/component/AddOperation.java
@@ -1,19 +1,25 @@
 package com.edaakyil.spring.lib.operation.component;
 
 import com.edaakyil.spring.lib.operation.IIntOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class AddOperation implements IIntOperation {
     @Override
     public int applyAsInt(int left, int right)
     {
+        log.warn("AddOperation::applyAsInt invoked via: {}, {}", left, right);
+
         return left + right;
     }
 
     @Override
     public boolean isValid(char op)
     {
+        log.warn("AddOperation::applyAsInt invoked via: {}", op);
+
         return op == '+';
     }
 }
