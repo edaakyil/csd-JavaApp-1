51e855c6c862f4ac30b044c0bccc184ae30ca44a
edaakyil <edaakyil@gmail.com>
2025-12-01 15:02:51 +0300

    Update

---
+++ Projects/012-SpringBoot/FlightSystemApp/RepositoryLib/src/main/java/com/edaakyil/flight/data/dal/FlightSystemDataHelper.java
@@ -0,0 +1,131 @@
+package com.edaakyil.flight.data.dal;
+
+import com.edaakyil.flight.data.entity.City;
+import com.edaakyil.flight.data.entity.Country;
+import com.edaakyil.flight.data.repository.IAirportRepository;
+import com.edaakyil.flight.data.repository.ICityRepository;
+import com.edaakyil.flight.data.repository.ICountryRepository;
+import com.edaakyil.flight.data.repository.IFlightRepository;
+import com.edaakyil.java.lib.data.repository.exception.RepositoryException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+/*
+* DAL katmanının avantajı (görevi):
+*   - exception handling yapıyor
+*   - Repository katmanındaki işleri bir üst katman olan Service katmanı için yumuşatması.
+*     Böylelikle Service katmanı, Repository katmanındaki interface'lerin hepsini enjekte etmek yerine bir tek bu
+*     helper sınıfını enjekte edip bu helper sınıfının ilgili metotlarını çağırıyor. (Bu facade tasarım kalıbıdır.)
+*/
+
+@Component
+@Lazy
+@Slf4j
+public class FlightSystemDataHelper {
+    private final ICityRepository m_cityRepository;
+    private final IFlightRepository m_flightRepository;
+    private final ICountryRepository m_countryRepository;
+    private final IAirportRepository m_airportRepository;
+
+    public FlightSystemDataHelper(ICityRepository cityRepository, IFlightRepository flightRepository,
+                                  ICountryRepository countryRepository, IAirportRepository airportRepository)
+    {
+        m_cityRepository = cityRepository;
+        m_flightRepository = flightRepository;
+        m_countryRepository = countryRepository;
+        m_airportRepository = airportRepository;
+    }
+
+    public Optional<Country> findCountryById(long id)
+    {
+        try {
+            log.info("FlightSystemDataHelper.findCountryById -> id: {}", id);
+
+            return m_countryRepository.findById(id);
+
+        } catch(Throwable ex) {
+            log.error("FlightSystemDataHelper.findCountryById -> id: {}, Exception: {}, Message: {}",
+                    id, ex.getClass().getSimpleName(), ex.getMessage());
+
+            throw new RepositoryException("FlightSystemDataHelper.findCountryById", ex);
+        }
+    }
+
+    public Optional<City> findCityById(long id)
+    {
+        try {
+            log.info("FlightSystemDataHelper.findCityById -> id: {}", id);
+
+            return m_cityRepository.findById(id);
+
+        } catch(Throwable ex) {
+            log.error("FlightSystemDataHelper.findCityById -> id: {}, Exception: {}, Message: {}",
+                    id, ex.getClass().getSimpleName(), ex.getMessage());
+
+            throw new RepositoryException("FlightSystemDataHelper.findCityById", ex);
+        }
+    }
+
+    public Iterable<Country> findAllCountries()
+    {
+        try {
+            log.info("FlightSystemDataHelper.findAllCountries");
+
+            return m_countryRepository.findAll();
+
+        } catch (Throwable ex) {
+            log.error("FlightSystemDataHelper.findAllCountries -> Exception: {}, Message: {}",
+                    ex.getClass().getSimpleName(), ex.getMessage());
+
+            throw new RepositoryException("FlightSystemDataHelper.findAllCountries", ex);
+        }
+    }
+
+    public Iterable<City> findAllCities()
+    {
+        try {
+            log.info("FlightSystemDataHelper.findAllCities");
+
+            return m_cityRepository.findAll();
+
+        } catch (Throwable ex) {
+            log.error("FlightSystemDataHelper.findAllCities -> Exception: {}, Message: {}",
+                    ex.getClass().getSimpleName(), ex.getMessage());
+
+            throw new RepositoryException("FlightSystemDataHelper.findAllCities", ex);
+        }
+    }
+
+    public Country saveCountry(Country country)
+    {
+        try {
+            log.info("FlightSystemDataHelper.saveCountry -> Country: {}", country.toString());
+
+            return m_countryRepository.save(country);
+
+        } catch (Throwable ex) {
+            log.error("FlightSystemDataHelper.saveCountry -> Country: {}, Exception: {}, Message: {}",
+                    country.toString(), ex.getClass().getSimpleName(), ex.getMessage());
+
+            throw new RepositoryException("FlightSystemDataHelper.saveCountry", ex);
+        }
+    }
+
+    public City saveCity(City city)
+    {
+        try {
+            log.info("FlightSystemDataHelper.saveCity -> City: {}", city.toString());
+
+            return m_cityRepository.save(city);
+
+        } catch (Throwable ex) {
+            log.error("FlightSystemDataHelper.saveCity -> City: {}, Exception: {}, Message: {}",
+                    city.toString(), ex.getClass().getSimpleName(), ex.getMessage());
+
+            throw new RepositoryException("FlightSystemDataHelper.saveCity", ex);
+        }
+    }
+}
