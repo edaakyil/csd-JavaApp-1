757cb692436addc9efaa9eff4ee099e33b4616b4
edaakyil <edaakyil@gmail.com>
2025-12-05 16:43:23 +0300

    Update

--- Projects/012-SpringBoot/FlightSystemApp/RepositoryLib/src/main/java/com/edaakyil/flight/data/entity/City.java
+++ Projects/012-SpringBoot/FlightSystemApp/RepositoryLib/src/main/java/com/edaakyil/flight/data/entity/City.java
@@ -7,7 +7,7 @@ import lombok.experimental.Accessors;
 @Setter
 @ToString
 @AllArgsConstructor
-@EqualsAndHashCode(exclude = {"m_name", "m_countryId"})
+@EqualsAndHashCode(exclude = {"m_name", "m_countryId"})  // Sadece id'si eşit olan City'ler aynı kabul edilecek
 public class City {
     @Accessors(prefix = "m_")
     private long m_id;
---
+++ Projects/012-SpringBoot/FlightSystemApp/RepositoryLib/src/test/java/com/edaakyil/flight/data/CityRepositorySaveTest.java
@@ -0,0 +1,33 @@
+package com.edaakyil.flight.data;
+
+import com.edaakyil.flight.data.entity.City;
+import com.edaakyil.flight.data.repository.ICityRepository;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
+@Disabled
+public class CityRepositorySaveTest {
+    @Autowired
+    private ICityRepository m_cityRepository;
+
+    @Test
+    public void givenValue_whenCity_thenSaveAndFindById()
+    {
+        var city = new City(0, "Zonguldak", 309);
+        var expectedId = 1001L;
+
+        m_cityRepository.save(city);
+
+        var cityOpt = m_cityRepository.findById(expectedId);
+
+        assertEquals(expectedId, cityOpt.get().getId());
+    }
+}
